5dce8946530940334b05ed69dd876a0dd670eaa9
Ivana Sovilj <isovilj11@outlook.com>
2020-05-04 22:01:51 +0200

    add in new repo

--- Dining-philosophers problem/src/Philosopher.java
+++
@@ -1,9 +0,0 @@
-public class Philosopher extends Thread {
-
-    Table table;
-
-    public Philosopher(String name, int leftFork, int rightFork, Table table) {
-        this.table = table;
-    }
-
-}
